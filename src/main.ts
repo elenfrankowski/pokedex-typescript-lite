ca2a25d43284c50b6fb91d08f4b703af8b4fc62b
Elen Frankowski <frankowski.elen@gmail.com>
2026-06-03 15:58:34 -0300

    feat: implementar loop de menu interativo na pasta views

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
-console.log("Pokédex TypeScript Lite inicializada com sucesso!");
+import { MenuView } from './views/MenuView.js';
+
+console.log('🚀 Inicializando Pokédex CLI...');
+
+const menu = new MenuView();
+menu.iniciar();
